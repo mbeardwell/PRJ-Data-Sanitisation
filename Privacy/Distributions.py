9231926d88a75dc744b68497c798f9bd212168f0
mbeardwell <kraafta37@gmail.com>
2022-04-28 19:48:36 +0100

    Replace all_patterns{_dual, _dual_rec, _rec, etc.}()

--- Privacy/Distributions.py
+++ Privacy/Distributions.py
@@ -80,12 +80,18 @@ class Distribution(JointDistribution):
 class FrequencyDistribution(Distribution):
     def __init__(self, sequence):
         super().__init__()
-        possible_patterns = all_patterns(sequence)
+
+        # enumerate all subsequences of 'sequence'
+        possible_patterns = []
+        for index_array in enum_all_subsequence_indices(len(sequence)):
+            possible_patterns.append(get_subsequence(sequence, index_array))
+
         for pattern in possible_patterns:  # list(set(..)) returns all unique patterns
             try:
                 self[pattern] += 1
             except KeyError:
                 self[pattern] = 1
+
 
 class ProbabilityDistribution(Distribution):
     # FIXME don't use the frequency distribution
@@ -101,12 +107,15 @@ class ProbabilityDistribution(Distribution):
 class JointFrequencyDistribution(JointDistribution):
     def __init__(self, sequence1, sequence2):
         super().__init__()
-        possible_patterns = all_patterns_dual(sequence1, sequence2)
-        for pattern in possible_patterns:
-            try:
-                self.set(self.get(pattern) + 1, pattern)
-            except KeyError:
-                self.set(0, pattern)
+        print(f"x = JointFrequencyDistribution({sequence1},{sequence2})")
+        # TODO
+
+        # possible_patterns = all_patterns_dual(sequence1, sequence2)
+        # for pattern in possible_patterns:
+        #     try:
+        #         self.set(self.get(pattern) + 1, pattern)
+        #     except KeyError:
+        #         self.set(1, pattern)
 
 
 class JointProbabilityDistribution(JointFrequencyDistribution):
@@ -118,32 +127,66 @@ class JointProbabilityDistribution(JointFrequencyDistribution):
                 self.hash_to_val[hash] = self.hash_to_val[hash] / total
 
 
-# TODO completely rewrite
-# TODO https://www.geeksforgeeks.org/generating-all-possible-subsequences-using-recursion/
-def all_patterns_rec_dual(input_sequence, generalised_sequence, i, input_seq_pats, output_seq_pats):
-    if i == len(input_sequence):
-        return [input_seq_pats]
-    else:
-        return all_patterns_rec(input_sequence, i + 1, input_seq_pats, output_seq_pats) + \
-               all_patterns_rec(input_sequence, i + 1, input_seq_pats + [input_sequence[i]],
-                                generalised_sequence + [generalised_sequence[i]])
+# returns all subsequence indices of a sequence
+# e.g. for a sequence of length 3: [[0],[1],[2],[0,1],[0,2],[1,2],[0,1,2]]
+# uses binary numbers where each number says whether each index is to be included in the output array
+# i.e. '0b101' means index array [0,2]
+def enum_all_subsequence_indices(array_len):
+    subseq_indices = []
+    counter = bin(1)  # don't start from 0 otherwise empty array [] will be included
 
+    def incr_counter(c):
+        return bin(int(c, 2) + 1)
 
-def all_patterns_rec(input_sequence, i, input_seq_pats):
-    if i == len(input_sequence):
-        return [input_seq_pats]
-    else:
-        return all_patterns_rec(input_sequence, i + 1, input_seq_pats) + \
-               all_patterns_rec(input_sequence, i + 1, input_seq_pats + [input_sequence[i]])
+    # e.g. '0b101' --> [0,2] or '0b111' --> [0,1,2]
+    def bin_to_indices(num_bin: bin):
+        indices = []
+        num_bin = num_bin[2:]  # remove '0b' from beginning of string
+        for i in range(len(num_bin)):
+            digit = num_bin[i]
+            if digit == '1':
+                indices.append(i)
+        return indices
 
+    for i in range(2 ** array_len):
+        subseq_indices.append(bin_to_indices(counter))
+        counter = incr_counter(counter)
 
-def all_patterns_dual(input_sequence, generalised_sequence):
-    return all_patterns_rec_dual(input_sequence, generalised_sequence, 0, [], [])
+    return subseq_indices
 
-
-def all_patterns(sequence):
-    patterns = all_patterns_rec(sequence, 0, [])
-    # # remove the empty pattern
-    # if [] in patterns:
-    #     patterns.remove([])
-    return patterns
+# e.g. indices = [1,4,7] returns [sequence[1], sequence[4], sequence[7]]
+def get_subsequence(sequence, indices):
+    return [sequence[i] for i in indices]
+#
+# # TODO completely rewrite
+# # TODO https://www.geeksforgeeks.org/generating-all-possible-subsequences-using-recursion/
+# def all_patterns_rec(input_sequence, i, input_seq_pats):
+#     if i == len(input_sequence):
+#         return [input_seq_pats]
+#     else:
+#         return all_patterns_rec(input_sequence, i + 1, input_seq_pats) + \
+#                all_patterns_rec(input_sequence, i + 1, input_seq_pats + [input_sequence[i]])
+#
+#
+# def all_patterns(sequence):
+#     print("Single sequence:", sequence)
+#     patterns = all_patterns_rec(sequence, 0, [])
+#     # # remove the empty pattern
+#     # if [] in patterns:
+#     #     patterns.remove([])
+#     return patterns
+#
+#
+# def all_patterns_dual_rec(input_sequence, generalised_sequence, i, input_seq_pats, output_seq_pats):
+#     print(input_seq_pats, "\n", output_seq_pats, "\n\n")
+#     if i == len(input_sequence):
+#         return [input_seq_pats]
+#     else:
+#         return all_patterns_dual_rec(input_sequence, generalised_sequence, i + 1, input_seq_pats, output_seq_pats) + \
+#                all_patterns_dual_rec(input_sequence, generalised_sequence, i + 1, input_seq_pats + [input_sequence[i]],
+#                                      generalised_sequence + [generalised_sequence[i]])
+#
+#
+# def all_patterns_dual(input_sequence, generalised_sequence):
+#     print("Two sequences:", input_sequence, "\n", generalised_sequence)
+#     return all_patterns_dual_rec(input_sequence, generalised_sequence, 0, [], [])
